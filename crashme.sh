0a01ee4120cb5b6236984261eb5cd3f85fc6b81a
Nathan Henrie <nate@n8henrie.com>
2022-05-03 13:11:12 -0600

    Reset `src/main.rs` before and after running

--- crashme.sh
+++ crashme.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 # https://github.com/NixOS/nixpkgs/issues/162795
 
-git checkout src/main.rs
+cleanup() {
+  git checkout src/main.rs
+}
+trap cleanup EXIT
 
+git checkout src/main.rs
 cargo clean
 i=0
 while cargo run; do
-  sed -i "s/world[0-9]*/world$((i++))/" src/main.rs
+  sed -i "s/world[0-9]*/world$((++i))/" src/main.rs
 done
