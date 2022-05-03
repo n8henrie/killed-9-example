7c5caea57c2753beca78586fd012306a6e6bd793
Nathan Henrie <nate@n8henrie.com>
2022-05-03 13:11:39 -0600

    Try flakes

--- flake.nix
+++ flake.nix
@@ -1,16 +1,32 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
-pkgs.mkShell {
-  nativeBuildInputs = [
-    cargo
-    cargo-edit
-    git
-    libiconv
-    openssl
-    pkg-config
-    rust-analyzer
-    rustc
-    rustfmt
-    darwin.apple_sdk.frameworks.Security
-  ];
+{
+  description = "Test rust in nix";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/22.05-pre";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+    in
+    {
+      devShell.${system} =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        mkShell {
+          nativeBuildInputs = [
+            cargo
+            cargo-edit
+            git
+            libiconv
+            openssl
+            pkg-config
+            rust-analyzer
+            rustc
+            rustfmt
+            darwin.apple_sdk.frameworks.Security
+          ];
+        };
+    };
 }
